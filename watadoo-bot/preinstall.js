87764acd6b47b45d5b0d7a7c540c6dc3d03f190f
Félix <the2deux@gmail.com>
2019-11-08 12:39:20 -0500

    Cleaning package.json file

--- watadoo-bot/preinstall.js
+++ watadoo-bot/preinstall.js
@@ -1,1 +1,3 @@
-const fs = require("fs"); fs.writeFile("./google-credentials-heroku.json", process.env.GOOGLE_CONFIG, () => {});
+// Solution found here: https://medium.com/@nwkeeley/a-better-solution-would-be-to-4fde38db8401
+const fs = require("fs");
+fs.writeFile("./google-credentials-heroku.json", process.env.GOOGLE_CONFIG, () => {});
